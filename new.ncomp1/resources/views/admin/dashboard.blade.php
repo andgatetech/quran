5d1ae9bd3dd54d0e198c4962fbcdbfe869b73dfe
Khorshed Alam <andgatetech@gmail.com>
2025-03-25 00:14:09 +0600

    remove all files to reconfigure

--- new.ncomp1/resources/views/admin/dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('admin.layout.app')
-@section('content')
-<div class="container">
-    <h2>Dashboard</h2>
-    <p>Welcome to the Admin Panel Dashboard!</p>
-</div>
-@endsection
--- resources/views/admin/layout/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="text-center py-3">
-    <p>&copy; 2023 Admin Panel. All rights reserved.</p>
-</footer>
