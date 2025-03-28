57d99338a4e7fab3dd960f717d558ac74e97513d
Khorshed Alam <andgatetech@gmail.com>
2025-03-28 23:23:48 +0600

    new route file for poetry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use FontLib\Table\Type\name;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -9,6 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function(){
+            Route::prefix('client/poetry')->name('poetry')->group(base_path('routes/poetry.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
